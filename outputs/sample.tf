d484c4c6f54e2cddd65096cfc532b79be9079922
USER <hameed.momin@gmail.com>
2021-10-11 18:05:07 +0530

    terraform

--- outputs/sample.tf
+++ outputs/sample.tf
@@ -4,9 +4,16 @@ output "sample" {
 }
 
 output "sample1" {
-  value = "${var.sample1}"
+  value = "${var.sample1} hameed"
 }
 
 variable "sample1" {
   default = "Myself"
 }
+
+
+## Variable sample can be accessed as var.sample or ${var.sample}, However
+## var.sample is used without quotes
+## ${var.sample} is used within quotes.
+## Usually ${} used only when you combine with some other data
+## Single quotes does not support by terraform at any level. Supports only double quotes.
