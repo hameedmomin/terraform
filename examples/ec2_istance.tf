4f6ead51b2f36b035f46f3dd03c05780fd5ff1e7
USER <hameed.momin@gmail.com>
2021-10-13 10:11:14 +0530

    terraform

--- examples/ec2_istance.tf
+++ examples/ec2_istance.tf
@@ -2,3 +2,10 @@ resource "aws_instance" "sample" {
   ami               = "ami-074df373d6bafa625"
   instance_type     = "t2.micro"
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+output "public_ip" {
+  value = aws_instance.sample.public_ip
+}
