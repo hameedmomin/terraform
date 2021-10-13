4675c65aef166b40ed0b814f23bed42f448a171a
USER <hameed.momin@gmail.com>
2021-10-13 19:43:07 +0530

    terraform

--- loops/sample.tf
+++ loops/sample.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "sample" {
   count                   = 2
   ami                     = "ami-074df373d6bafa625"
   instance_type           = "t2.micro"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
