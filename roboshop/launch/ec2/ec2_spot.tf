2676ff1faed033d52967e3c28f09a208e1829249
USER <hameed.momin@gmail.com>
2021-10-14 18:55:19 +0530

    terraform

---
+++ roboshop/launch/ec2/ec2_spot.tf
@@ -0,0 +1,9 @@
+resource "aws_spot_instance_request" "mywork" {
+  ami           = "ami-074df373d6bafa625"
+  spot_price    = "0.0035"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mywork"
+  }
+}
