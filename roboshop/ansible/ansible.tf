06e16ced3e914d00c8f151216974fb41c02e1887
USER <hameed.momin@gmail.com>
2021-10-17 20:04:58 +0530

    terraform

--- roboshop/ansible/ansible.tf
+++ roboshop/ansible/ansible.tf
@@ -1,7 +1,8 @@
 resource "null_resource" "ansible" {
 
   count               = length(var.COMPONENTS)
-      provisioner "remote-exec" {
+
+  provisioner "remote-exec" {
 
     connection {
       host            = "${element(var.COMPONENTS, count.index)}.connection.internal"
@@ -10,9 +11,9 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-      "sudo yum install ansible -y",
-      "sudo yum remove ansible -y",
-      "sudo pip install ansible==3.4.0",
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible==4.1.0",
       "ansible-pull -i localhost, -U https://github.com/hameedmomin/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
--- roboshop/launch/templates.tf
+++ roboshop/launch/templates.tf
@@ -1,23 +1,23 @@
 resource "aws_launch_template" "new" {
-  name = "new"
-  image_id = "ami-074df373d6bafa625"
+  name                       = "new"
+  image_id                   = "ami-074df373d6bafa625"
 
   instance_market_options {
-    market_type = "spot"
+    market_type              = "spot"
   }
 
-  instance_type = "t2.micro"
+  instance_type              = "t2.micro"
 
 
   placement {
-    availability_zone = "us-east-1"
+    availability_zone         = "us-east-1"
   }
 
 
-  vpc_security_group_ids = ["sg-0b9ab8f976b104516"]
+  vpc_security_group_ids       = ["sg-0b9ab8f976b104516"]
 
 }
 
 output "template_id" {
-  value = "aws_launch_template.new_id"
+  value                        = "aws_launch_template.new_id"
 }
