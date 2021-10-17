8002a319275a630bb2e062aa5a6b277c4abb2782
USER <hameed.momin@gmail.com>
2021-10-17 18:34:11 +0530

    terraform

---
+++ roboshop/ansible/ansible.tf
@@ -0,0 +1,22 @@
+resource "null_resource" "ansible" {
+  count          = length(var.COMPONENTS)
+  provisioner "remote-exec" {
+
+    connection {
+      host       = "${elements(var.COMPONENTS, count.index)}.connection.internal"
+      user       = "centos"
+      password   = "DevOps321"
+    }
+
+    inline = [
+      "sudo yum install ansible -y",
+      "sudo yum remove ansible -y",
+      "sudo pip install ansible==3.4.0",
+      "ansible-pull -i localhost, -U https://github.com/hameedmomin/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+    ]
+
+  }
+
+}
+
+variable "COMPONENTS" {}
