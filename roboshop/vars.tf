e98f4890b32aea8717cee7888df1e5eed16891a1
USER <hameed.momin@gmail.com>
2021-11-07 18:25:23 +0530

    terraform

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -1,3 +1,8 @@
+//variable "COMPONENTS" {
+ // default      = [ "frontend", "mongodb", "catalogue", "mysql", "rabbitmq", "user", "payment", "shipping", "redis", "cart" ]
+//}
+
 variable "COMPONENTS" {
-  default      = [ "frontend", "mongodb", "catalogue", "mysql", "rabbitmq", "user", "payment", "shipping", "redis", "cart" ]
+  default     = cart
+
 }
